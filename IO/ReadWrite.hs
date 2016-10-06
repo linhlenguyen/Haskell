34c284213bd5afac786ea786095cb219460337d4
linh <linh.lenguyen@vitalpac.com>
2016-10-06 19:11:32 +0100

    Stuffs

--- IO/ReadWrite.hs
+++ IO/ReadWrite.hs
@@ -1,10 +1,4 @@
 import System.Environment
 
-main' :: IO ()
-main' = getArgs >>= (\[arg1,arg2] -> putStrLn arg1 >> putStrLn arg2)
-
-getInput :: Char
-getInput = getChar
-
 main :: IO ()
-main = 
+main = getArgs >>= (\[arg1,arg2] -> putStrLn arg1 >> putStrLn arg2)
